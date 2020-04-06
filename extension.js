066018be4a3637213e7d816f6646c07cfdc896a8
nondanee <iminezn5656@outlook.com>
2020-04-06 15:52:22 +0800

    optimize activate action

--- extension.js
+++ extension.js
@@ -3,12 +3,15 @@ const runtime = require('./runtime.js')
 const package = require('./package.json')
 
 const activate = context => {
-	context.subscriptions.push(vscode.commands.registerCommand('neteasemusic.start', () => runtime.activate(context)))
-	context.subscriptions.push(vscode.commands.registerCommand('neteasemusic.stop', () => runtime.dispose()))
-	context.subscriptions.push(vscode.commands.registerCommand('neteasemusic.reset', () => {
-		Object.keys(package.contributes.configuration.properties).forEach(key => vscode.workspace.getConfiguration().update(key, undefined, vscode.ConfigurationTarget.Global))
-		Object.keys(context.globalState._value).forEach(key => context.globalState.update(key, undefined))
-	}))
+	const action = {
+		start: () => runtime.activate(context),
+		stop: () => runtime.dispose(),
+		reset: () => {
+			Object.keys(package.contributes.configuration.properties).forEach(key => vscode.workspace.getConfiguration().update(key, undefined, vscode.ConfigurationTarget.Global))
+			Object.keys(context.globalState._value).forEach(key => context.globalState.update(key, undefined))
+		}
+	}
+	Object.keys(action).forEach(name => context.subscriptions.push(vscode.commands.registerCommand(`neteasemusic.${name}`, action[name])))
 }
 exports.activate = activate
 
